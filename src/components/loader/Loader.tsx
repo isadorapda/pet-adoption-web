3876d8f5aba726fa8df297de4daa6a39b922aa0f
Isadora Pilau <isadorapda@gmail.com>
2023-06-03 15:03:32 +0100

    feat: 🎸 add loader and changed logo navbar

---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,11 @@
+import Logo from '@/assets/paw.webp'
+
+export function Loader() {
+  return (
+    <div className="w-screen h-screen absolute top-0 left-0 flex flex-col justify-center items-center bg-[rgba(255,_255,_255,_0.7)] z-50">
+      <div className="h-[20vh] animate-bounce flex flex-col justify-center items-center">
+        <img src={Logo} alt="" className=" w-[10vw]" />
+      </div>
+    </div>
+  )
+}
