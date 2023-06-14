9564d7af76ca5f166d2e79d633301cd1609715a1
Isadora Pilau <isadorapda@gmail.com>
2023-06-14 19:35:10 +0100

    feat: 🎸 create different pet profile for orgs and pet seekers

--- src/pages/ViewPetDetails.tsx
+++ src/pages/ViewPetDetails.tsx
@@ -1,14 +1,11 @@
 import { useEffect, useState } from 'react'
 import { api } from '@/lib/axios'
 import { useParams } from 'react-router-dom'
-import { MdWhatsapp as IconWhatsApp } from 'react-icons/md'
 import { GrLocation as IconLocation } from 'react-icons/gr'
 import { Organisation, Pet } from '@/@types/models'
 import usePetsContext from '@/hooks/usePetsContext'
-import {
-  WHATSAPP_BASE_URL,
-  getPreFilledMessage,
-} from '@/constants/contactWhatsApp'
+
+import { EnquireAboutPet } from '@/components/petDetails/EnquireAboutPet'
 
 interface PetResponse {
   pet: Pet
@@ -20,7 +17,7 @@ interface Response<T> {
   data: T
 }
 
-export function PetDetails() {
+export function ViewPetDetails() {
   const { pets } = usePetsContext()
   const [pet, setPet] = useState<Pet>()
   const [org, setOrg] = useState<Organisation>()
@@ -31,15 +28,19 @@ export function PetDetails() {
       return api.get(`/pets/${params.petId}/details`)
     }
     function fecthOrg(): Promise<Response<OrgResponse>> {
-      const orgId = pets.find((pet) => pet.id === `${params.petId}`)
+      const findPet = pets.find((pet) => pet.id === `${params.petId}`)
 
-      return api.get(`/organisations/${orgId?.organisation_id}`)
+      return api.get(
+        `/organisations/${
+          params.orgId ? params.orgId : findPet?.organisation_id
+        }`,
+      )
     }
     Promise.all([fetchPet(), fecthOrg()]).then(async ([petRes, orgRes]) => {
       setPet(petRes.data.pet)
       setOrg(orgRes.data.organisation)
     })
-  }, [])
+  }, [params.orgId, params.petId, pets])
 
   if (!pet) {
     return null
@@ -90,27 +91,8 @@ export function PetDetails() {
               <p>{pet.ideal_home}</p>
             </div>
           </div>
-          <div className="rounded-lg bg-white p-6 flex flex-col shadow-card h-max gap-6">
-            <h1 className="font-bold ">
-              Could you be {pet.name} perfect match?
-            </h1>
-            <p>
-              {pet.name} is registered at {org.name}
-            </p>
-            <button className="button-primary bg-green">
-              <a
-                href={`${WHATSAPP_BASE_URL}${org.mobile}${getPreFilledMessage(
-                  pet.name,
-                )}`}
-                target="_blank"
-                rel="noreferrer"
-                className="flex items-center justify-center gap-5"
-              >
-                <IconWhatsApp /> Enquire about {pet.name}
-              </a>
-            </button>
-          </div>
         </div>
+        <EnquireAboutPet pet={pet} org={org} />
         <div>
           <h2>About {pet.name}</h2>
           <p>{pet.description}</p>
